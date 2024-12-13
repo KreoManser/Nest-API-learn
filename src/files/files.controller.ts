808c7f9560f9795aad7011693cca7829b4bdfc62
Sergey Babich <sherba.ru@icloud.com>
2024-12-13 23:44:34 +0300

    add file convert img to webp method

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -10,6 +10,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { FileElementResponseDto } from './dto/file-element.response.dto';
 import { FilesService } from './files.service';
+import { MFile } from './mfile.class';
 
 @Controller('files')
 export class FilesController {
@@ -20,6 +21,13 @@ export class FilesController {
   @UseGuards(JwtAuthGuard)
   @UseInterceptors(FileInterceptor('files'))
   async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<FileElementResponseDto[]> {
-    return await this.filesService.saveFiles([file]);
+    const saveArray: MFile[] = [new MFile(file)];
+    if (file.mimetype.includes('image')) {
+      const buffer = await this.filesService.convertToWebp(file.buffer);
+      saveArray.push(
+        new MFile({ originalname: `${file.originalname.split('.')[0]}.webp`, buffer }),
+      );
+    }
+    return await this.filesService.saveFiles(saveArray);
   }
 }
