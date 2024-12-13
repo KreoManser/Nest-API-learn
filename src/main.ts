d4572f869a26a65daed08e049f64a02b725fb53e
Sergey Babich <sherba.ru@icloud.com>
2024-12-14 01:59:10 +0300

    fix telegram api

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
+
+  const server = app.getHttpAdapter().getInstance();
+  const routes = server._router.stack.filter((layer) => layer.route).map((layer) => layer.route);
+  console.log('Registered Routes:', routes);
 }
 bootstrap();
