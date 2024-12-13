175c4d9d427376c572831cc2a769500209cc3ec8
Sergey Babich <sherba.ru@icloud.com>
2024-12-13 23:02:41 +0300

    add uploadFile method

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { getMongoConfig } from './configs/mongo.config';
+import { FilesModule } from './files/files.module';
 
 @Module({
   // eslint-disable-next-line prettier/prettier
@@ -24,6 +25,7 @@ import { getMongoConfig } from './configs/mongo.config';
     ProductModule,
     ReviewModule,
     UsersModule,
+    FilesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
